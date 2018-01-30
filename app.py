e8da74e61999cb27f3d2a7ccef41d63ecd6c1376
adis18-meet <adistoler13@gmail.com>
2018-01-30 17:16:12 +0200

    SDf

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask 
 from flask import render_template
 app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+class potato(db.Model):
+	__tablename__ = "potato"
+	id = db.Column(db.Integer,primary_key=True)
+	username = db.Column(db.String(25))
+	password = db.Column(db.String(20))
+db.create_all()
+
 
 @app.route("/")
 @app.route("/home")
